d82bb5ebb375dd86eb81db8a153d57f7a9690ea5
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-10-13 23:24:33 -0500

    CustomDialogFragment

---
+++ app/src/main/java/dialogfragment/custom/anguiano/carlos/customdialogfragment/FullScreenDialog.java
@@ -0,0 +1,45 @@
+package dialogfragment.custom.anguiano.carlos.customdialogfragment;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Carlos Anguiano on 13/10/17.
+ * For more info contact: c.joseanguiano@gmail.com
+ */
+
+public class FullScreenDialog extends DialogFragment {
+
+
+    public static final String TAG = "FullScreenDialog";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialogStyle);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle state) {
+        super.onCreateView(inflater, parent, state);
+
+        View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_layout, parent, false);
+        return view;
+    }
+}
